b6001abe657515364e98ea99826b520d984ff211
2realzoo <realzoojin@gmail.com>
2024-09-10 11:33:48 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 516 ms, Memory: 127672 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,21 +1,51 @@
+# import sys
+
+# input = sys.stdin.readline
+
+# N = int(input().rstrip())
+# n_card = list(map(int, input().rstrip().split()))
+# _ = input()
+# m_card = list(map(int, input().rstrip().split()))
+
+# n_card_dict = {m:0 for m in m_card}
+# for n in n_card:
+#     if not n in n_card_dict:
+#         n_card_dict[n] = 1
+#     else:
+#         n_card_dict[n] += 1
+
+# result = []
+# for m in m_card:
+#     result.append(n_card_dict[m])
+
+# print(*result)
+
+# 숏코드 - dictionary 활용
+# import sys
+
+# input = sys.stdin.readline
+
+# _ = input()
+# n_card = input().rstrip().split()
+# d = dict.fromkeys(set(n_card), 0)
+
+# for n in n_card:
+#     d[n] += 1
+
+# _ = input()
+# m_card = input().rstrip().split()
+
+# print(*[d.get(m, 0) for m in m_card])
+
+# 숏코드 - Counter 활용
 import sys
+from collections import Counter
 
 input = sys.stdin.readline
 
-N = int(input().rstrip())
-n_card = list(map(int, input().rstrip().split()))
-M = int(input().rstrip())
-m_card = list(map(int, input().rstrip().split()))
+input()
+n_card = Counter(input().split())
+input()
+# Counter dictionary는 누락된 항목에 대해 keyerror를 발생시키는 대신 0 반환
+print(*[n_card[m] for m in input().split()])
 
-n_card_dict = {m:0 for m in m_card}
-for n in n_card:
-    if not n in n_card_dict:
-        n_card_dict[n] = 1
-    else:
-        n_card_dict[n] += 1
-
-result = []
-for m in m_card:
-    result.append(n_card_dict[m])
-
-print(' '.join(map(str, result)))
