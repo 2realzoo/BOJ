549ba1edfc774cf38d27841453a3eb5a269004de
2realzoo <realzoojin@gmail.com>
2024-10-23 21:35:18 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 484 ms, Memory: 39672 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -4,38 +4,31 @@ from collections import deque
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-visited = [[-1] * M for _ in range(N)]
-dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
-
-def bfs(i, j):
+map = [list(map(int, input().split())) for _ in range(N)]
+visited = [[-1]*M for _ in range(N)]
+dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
+def dfs(i, j):
     q = deque()
     q.append((i, j))
-    visited[i][j] = 0
-    
     while q:
         x, y = q.popleft()
-        
         for i in range(4):
-            nx, ny = dx[i] + x, dy[i] + y
-            
-            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == -1:
-                    if graph[nx][ny] == 0:
-                        visited[nx][ny] = 0
-                    elif graph[nx][ny] == 1:
-                        q.append((nx, ny))
-                        visited[nx][ny] = visited[x][y] + 1
+            nx, ny = dx[i]+x, dy[i]+y
+            if 0<=nx<N and 0<=ny<M and visited[nx][ny]==-1 and map[nx][ny]==1:
+                q.append((nx, ny))
+                visited[nx][ny] = visited[x][y] + 1
+                
+for n in range(N):
+    for m in range(M):
+        if map[n][m] == 2 and visited[n][m] == -1:
+            visited[n][m] = 0
+            dfs(n, m)
 
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 2 and visited[i][j] == -1:
-            bfs(i, j)
-                       
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 0:
-            print('0', end=' ')
+for n in range(N):
+    for m in range(M):
+        if map[n][m] == 0:
+            print(0, end=' ')
         else:
-            print(visited[i][j], end=' ')
+            print(visited[n][m], end=' ')
     print()
-       
+                
