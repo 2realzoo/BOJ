37da8d23a9d6338b490b461f04436115c27d2119
2realzoo <realzoojin@gmail.com>
2024-11-08 00:10:51 +0900

    [Silver III] Title: N과 M (5), Time: 208 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,14 +1,13 @@
 import sys
-
 input = sys.stdin.readline
 from itertools import permutations
 
 N, M = map(int, input().split())
-d = list(map(int, input().split()))
-d.sort()
-com_d = list(permutations(d, M))
-# com_d.sort()
-for i in com_d:
-    for m in range(M):
-        print(i[m], end=' ')
+nums = list(map(int, input().split()))
+nums.sort()
+per_nums = permutations(nums, M)
+
+for num in per_nums:
+    for i in range(M):
+        print(num[i], end=' ')
     print()
