fd1184e3809f692a58de7061fc894d052607fc96
2realzoo <realzoojin@gmail.com>
2025-04-04 15:52:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -7,11 +7,11 @@ graph = []
 for _ in range(n):
     graph.append(list(map(int, input().rstrip())))
 
-def bfs(graph, x, y):
+def bfs(x, y):
     q = deque()
     q.append((x, y))
     graph[x][y] = 0
-    house_num = 1
+    cnt = 1
 
     while q:
         x, y = q.popleft()
@@ -22,22 +22,22 @@ def bfs(graph, x, y):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx<0 or nx>=n or ny<0 or ny>=n:
+            if nx<0 or nx>= n or ny<0 or ny>=n:
                 continue
             if graph[nx][ny]==1:
-                house_num += 1
                 graph[nx][ny] = 0
+                cnt += 1
                 q.append((nx, ny))
-    
-    return house_num
+    return cnt
 
 complex = []
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
-            complex.append(bfs(graph, i, j))
+            complex.append(bfs(i, j))
 
 complex.sort()
 print(len(complex))
-for i in range(len(complex)):
-    print(complex[i])
+for c in complex:
+    print(c)
+            
